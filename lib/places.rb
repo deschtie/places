0ceebd6838eea696eaabb083e3e893d430232fca
Michael Wayne Hoeft & Desire Perrault-Reid <student@epicodus.com>
2015-04-28 10:57:44 -0700

    Add initial files, runs 2 rspecs successfully.

---
+++ lib/places.rb
@@ -0,0 +1,15 @@
+class Places
+  define_method(:initialize) do |description|
+    @description = description
+  end
+
+  define_method(:description) do
+    @description
+  end
+
+  define_singleton_method(:all) do
+    []
+  end
+
+  
+end
---
+++ spec/places_spec.rb
@@ -0,0 +1,20 @@
+require('rspec')
+require('places')
+
+describe(Places) do
+  describe("#description") do
+    it("lets user give it a description of where he/she has traveled") do
+    test_Adventure = Places.new("LA, CA")
+    expect(test_Adventure.description()).to(eq("LA, CA"))
+    end
+  end
+
+  describe(".empty_place") do
+    it("gives the user an empty array where he/she will post places traveled") do
+    expect(Places.all()).to(eq([]))
+    end
+  end
+
+
+  
+end
